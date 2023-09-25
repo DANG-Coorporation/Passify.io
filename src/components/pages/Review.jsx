11476234414f3e94d97ca7b6a8068bce91270eb6
Hilmiwi <wijaksanahilmi@gmail.com>
2023-09-25 16:32:12 +0700

    edit: convert to tailwind

--- src/components/pages/Review.jsx
+++ src/components/pages/Review.jsx
@@ -1,4 +1,3 @@
-import style from "../../style/Review.module.css";
 import { useState } from "react";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 export default function Review() {
   const [number, setNumber] = useState(0);
   const [hoverStar, setHoverStar] = useState(undefined);
+  const [comment, setComment] = useState('')
   const navigate = useNavigate();
 
   const handleText = () => {
@@ -45,54 +45,63 @@ export default function Review() {
         return "Comment Here...";
     }
   };
+  const handleSubmit = (event) => {
+    console.log('An essay was submitted: ' + comment + number);
+    event.preventDefault()
+  }
   return (
-    <div className={style.Review}>
-      <div className={style.popup}>
-        <div className={style.content}>
-          <div className={style.product}>
-            <h1>Rate us below !</h1>
+    <div className="text-center h-[65vh] w-screen overscroll-none">
+      <div className="fixed flex items-center justify-center top-0 left-0 right-0 bottom-0 bg-black bg-opacity-25">
+        <form>
+          <div className="flex p-5 flex flex-col items-center justify-center bg-white rounded-xl w-[300px] max-h-[50vh] ">
+            <h1 className="text-xl font-bold mb-5">Rate us below !</h1>
+            <div>
+              <p>
+                {handleText()}, {handlePlaceHolder()}
+              </p>
+            </div>
+            <div className="flex duration-500 cursor-pointer my-2 hover:-translate-y-1">
+              {Array(5)
+                .fill()
+                .map((_, index) =>
+                  number >= index + 1 || hoverStar >= index + 1 ? (
+                    <AiFillStar
+                      className="w-8 h-8"
+                      key={index}
+                      onMouseOver={() => setHoverStar(index + 1)}
+                      onMouseLeave={() => setHoverStar(undefined)}
+                      style={{ color: "orange" }}
+                      onClick={() => setNumber(index + 1)}
+                    />
+                  ) : (
+                    <AiOutlineStar
+                      className="w-8 h-8"
+                      key={index}
+                      onMouseOver={() => setHoverStar(index + 1)}
+                      onMouseLeave={() => setHoverStar(undefined)}
+                      style={{ color: "orange" }}
+                      onClick={() => setNumber(index + 1)}
+                    />
+                  )
+                )}
+            </div>
+            <textarea
+              className="w-full rounded-md p-2 h-40 border min-h-[50px]"
+              placeholder="comment here..."
+              id="comment"
+              onChange={(e)=> setComment(e.target.value)}
+            />
+            <div>
+              <input
+                type="submit"
+                className="bg-primaryColor text-white mt-2 rounded-md cursor-pointer py-2 px-3 hover:duration-500 hover:-translate-y-1 disabled:bg-gray-300"
+                onClick={(e)=>handleSubmit(e)}
+                disabled={number === 0}
+              >
+              </input>
+            </div>
           </div>
-          <div>
-            <p>
-              {handleText()}, {handlePlaceHolder()}
-            </p>
-          </div>
-          <div className={style.star}>
-            {Array(5)
-              .fill()
-              .map((_, index) =>
-                number >= index + 1 || hoverStar >= index + 1 ? (
-                  <AiFillStar
-                    key={index}
-                    onMouseOver={() => setHoverStar(index + 1)}
-                    onMouseLeave={() => setHoverStar(undefined)}
-                    style={{ color: "orange" }}
-                    onClick={() => setNumber(index + 1)}
-                  />
-                ) : (
-                  <AiOutlineStar
-                    key={index}
-                    onMouseOver={() => setHoverStar(index + 1)}
-                    onMouseLeave={() => setHoverStar(undefined)}
-                    style={{ color: "orange" }}
-                    onClick={() => setNumber(index + 1)}
-                  />
-                )
-              )}
-          </div>
-          <textarea className={style.place} placeholder="comment here..." />
-          <div>
-            <button
-              className={style.button}
-              onClick={() => {
-                navigate("/order-list");
-                alert("Thank you, see ya !");
-              }}
-            >
-              Submit
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
